f8103c74d9196223e3bcd8e7f785276928a89b6b
georgem03 <georgem0@byu.edu>
2026-04-03 21:12:40 -0600

    database mission13'

--- backend/Mission11_Martinez.API/Controllers/BookController.cs
+++ backend/Mission11_Martinez.API/Controllers/BookController.cs
@@ -55,5 +55,46 @@ public class BookController : ControllerBase
 
         return Ok(categories);
     }
+
+    [HttpPost("addbook")]
+    public IActionResult AddBook([FromBody] Book newBook)
+    {
+        _bookContext.Books.Add(newBook);
+        _bookContext.SaveChanges();
+        return Ok(newBook);
+    }
+
+    [HttpPut("updatebook/{bookId}")]
+    public IActionResult UpdateBook(int bookId, [FromBody] Book updatedBook)
+    {
+        var existingBook = _bookContext.Books.Find(bookId);
+        if (existingBook == null)
+            return NotFound();
+
+        existingBook.Title = updatedBook.Title;
+        existingBook.Author = updatedBook.Author;
+        existingBook.Publisher = updatedBook.Publisher;
+        existingBook.ISBN = updatedBook.ISBN;
+        existingBook.Classification = updatedBook.Classification;
+        existingBook.Category = updatedBook.Category;
+        existingBook.PageCount = updatedBook.PageCount;
+        existingBook.Price = updatedBook.Price;
+
+        _bookContext.SaveChanges();
+        return Ok(existingBook);
+    }
+
+    [HttpDelete("deletebook/{bookId}")]
+    public IActionResult DeleteBook(int bookId)
+    {
+        var book = _bookContext.Books.Find(bookId);
+        if (book == null)
+            return NotFound();
+
+        _bookContext.Books.Remove(book);
+        _bookContext.SaveChanges();
+        return NoContent();
+    }
 }
 }
+
